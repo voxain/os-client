0946cd6b7c0a44655233bd1a2e495e9a726aae14
Meow <erty2pop@gmail.com>
2020-05-28 10:13:09 -0400

    Update server.js

--- express/server.js
+++ express/server.js
@@ -13,9 +13,6 @@ router.get("/test", (req, res) => {
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-);
 
 module.exports = app;
 module.exports.handler = serverless(app);
