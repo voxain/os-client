d0e97ab997dece8fd1d72a7864beb1c38dbaf999
Meow <erty2pop@gmail.com>
2020-05-28 13:00:04 -0400

    move

--- express/server.js
+++ express/server.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 var find = require("find");
 
 const router = express.Router();
-router.get("/script.js", (req, res) => {
+/*router.get("/script.js", (req, res) => {
   let d = "";
   let file = req.query.fil || "/";
   if (!file.endsWith("/")) {
@@ -17,21 +17,8 @@ router.get("/script.js", (req, res) => {
     d = fs.readdirSync(file);
   }
 
-  find.file(__dirname, function (files) {
-    console.log(files.length);
-    res.send(
-      __filename +
-        " " +
-        __dirname +
-        "<br>" +
-        d +
-        "<br>" +
-        process.cwd() +
-        "<br>" +
-        files
-    );
-  });
-});
+  res.send(__filename + " " + __dirname + "<br>" + d + "<br>" + process.cwd());
+});*/
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
