6a5c3efbbab85b269c3ebc7a4824526d2be96ded
Meow <erty2pop@gmail.com>
2020-05-28 10:00:24 -0400

    remove methods

--- express/server.js
+++ express/server.js
@@ -12,8 +12,6 @@ router.get("/", (req, res) => {
   res.write("Hello.");
   res.end();
 });
-router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
-router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
