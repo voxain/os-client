ce4ac1af641f2fa7ecfecfbf25f8a3b107d1c1a9
Meow <erty2pop@gmail.com>
2020-05-28 10:08:45 -0400

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,15 +7,15 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const router = express.Router();
-router.use(express.static("express/public"));
 router.get("/test", (req, res) => {
   res.send("Hello.");
 });
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
-app.use(express.static("express/public"));
+app.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 module.exports = app;
 module.exports.handler = serverless(app);
