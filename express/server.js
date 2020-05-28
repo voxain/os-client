4b1a0a2f344ce746810673826c051cc38b82ab47
Meow <erty2pop@gmail.com>
2020-05-28 10:25:25 -0400

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,10 +6,9 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const router = express.Router();
-router.use(express.static("express/public"));
-//router.get("/", (req, res) => {
-//  res.send("Hello.");
-//});
+router.get("*", (req, res) => {
+  res.send(req.originalUrl);
+});
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
