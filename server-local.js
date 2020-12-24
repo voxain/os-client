ddb9a11bae1792be707d4534cae815aef9f50782
Meow <50887230+itzTheMeow@users.noreply.github.com>
2020-12-24 18:09:03 -0500

    remove netlify template

--- server-local.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const app = require("./express/server");
-app.listen(3000, () => console.log("Local app listening on port 3000!"));
-
-/* This file is part of the netlify functions template. */
