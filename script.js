d48135dbdc8c5e3bad0594bbf7a28a4585092307
TheCodingGuy <officialtcgmatt@gmail.com>
2020-12-24 02:19:27 -0500

    A big overhaul of some stuff (#3)
    * A big overhaul of some stuff
    
    Okay, when TCG decides to take over some, he decides it's a great idea to make things his way, so pls no get mad :scream:
    Changed a lot of stuff :P
    
    * More overhaul of overhauled stuff
    
    Okay, when TCG decides to take over some, he decides it's a great idea to make things his way, so pls no get mad 😱 ... and ends up needing to change more :stuck_out_tongue:
    Changed a lot of stuff (yet again) :P

--- script.js
+++ script.js
@@ -1,3 +1,91 @@
+// script.js
+// Author: Meow Developers
+// Copyright Meow Developers
+
+
+const meowos = {
+  error: false, // probably will fix this later, idk its 1 am in the morning, help
+  system: {
+    loggingcolors: { 
+      "Reset": "\x1b[37m", // \x1b[0m doesn't work for some reason; It's a nodejs thing
+    
+      "FgBlack": "\x1b[30m",
+      "FgRed": "\x1b[31m",
+      "FgGreen": "\x1b[32m",
+      "FgYellow": "\x1b[33m",
+      "FgBlue": "\x1b[34m",
+      "FgMagenta": "\x1b[35m",
+      "FgCyan": "\x1b[36m",
+      "FgWhite": "\x1b[37m",
+    
+      "BgBlack": "\x1b[40m",
+      "BgRed": "\x1b[41m",
+      "BgGreen": "\x1b[42m",
+      "BgYellow": "\x1b[43m",
+      "BgBlue": "\x1b[44m",
+      "BgMagenta": "\x1b[45m",
+      "BgCyan": "\x1b[46m",
+      "BgWhite": "\x1b[47m"
+    },
+    version: "0.1.0" //TODO: include proper versioning
+  }
+};
+
+// Cool Error Handler Script
+// Provided by Yours Truly, TCG
+// Version 1.0.0.0
+// Modified 12/24/2020
+
+meowos.ErrorHandler = function(location = "SYSTEM", error, reportToDevs = false) {
+  var errorData = ((error.message) ? error.message : error);
+  if (error.stack) errorData += `\nStacktrace: ${error.stack}`;
+  document.body.innerHTML =
+    "<style>"
+      + "html, body { width: 100%; height: 100%; }"
+      + "#errorDiv {  position: absolute; width: 100%; height: 100%; font-family: Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif; font-size: 20px; color: #23272A; /* color: #2C2F33; */ background-color: #FF9100; top: 0; left: 0; }"
+      + "#errorDiv span { font-size: 26px; /* color: #E91E63; */ }"
+      + "#errorDiv span#stop { font-size: 22px; }"
+      + "#errorDiv p#reported { display: none; }"
+    + "</style>"
+    + "<div id='errorDiv'>"
+      + "<span>A fatal error has occurred and MeowOS has stopped execution.</span>"
+      + "<p><span id='stop'>STOP Code: </span><span>" + errorData.split("\n")[0] + "</span><br><br>In order to continue MeowOS, you will need to reload MeowOS. Any progress on or within programs may have been lost.</p>"
+      + "<br><p id='reported'>This incident has been reported to the MeowOS Developers.</p>"
+    + "</div>";
+  meowos.log("X", location, errorData);
+
+  if (reportToDevs) {
+    // TODO: Report to the damn developers or something...
+    // ~ TCG 12/24/2020
+
+    document.getElementById("reported").style.display = "show";
+  }
+  meowos.error = true;
+};
+
+
+// Cool Logging Script
+// Also provided by Yours Truly, TCG
+// Version 1.0.0.0
+// Modified 12/23/2020
+
+meowos.log = function(type, location, message) { // This is crappy code
+  var loggingcolors = meowos.system.loggingcolors, typecolor = loggingcolors.FgWhite;
+
+  if (type == "X") typecolor = loggingcolors.FgRed; // X = Error
+  else if (type == "i") typecolor = loggingcolors.FgBlue; // i = Information
+  else if (type == "!") typecolor = loggingcolors.FgYellow; // ! = Warning
+  else if (type == "S") typecolor = loggingcolors.FgGreen; // S = Success
+
+  var locationcolor = loggingcolors.FgWhite;
+
+  if (location == "SYSTEM") locationcolor = loggingcolors.FgGreen;
+  else if (location == "PROGRAM LOADER") locationcolor = loggingcolors.FgMagenta;
+
+  console.log(`(${typecolor}${type}${loggingcolors.Reset}) [${locationcolor}${location}${loggingcolors.Reset}] ${message}`);
+
+};
+
 let requiredToLoad = 3; // set initial load requirement
 
 let programsInstalled = [
@@ -10,28 +98,56 @@ let programsInstalled = [
 ]; //TODO: make this dynamic based on installed.syscfg
 requiredToLoad += programsInstalled.length;
 programsInstalled.forEach((p) => {
-  $.get(`${APIURL}programs/${p}`, function (program) {
-    let css = program.style || "";
-    if (css) {
-      // set CSS for each program in the head tag
-      let style = document.createElement("style");
-      style.innerHTML = css;
-      style.setAttribute("os-for-program", p);
-      document.head.appendChild(style);
+  if (meowos.error) return;
+  meowos.log("i", "PROGRAM LOADER", `Loading '${p}'...`);
+
+  $.ajax({
+    type: "get",
+    url: `${APIURL}programs/${p}`,
+    success: function (program, text) {
+      if (meowos.error) return;
+
+      let css = program.style || "";
+      if (css) {
+        // set CSS for each program in the head tag
+        let style = document.createElement("style");
+        style.innerHTML = css;
+        style.setAttribute("os-for-program", p);
+        document.head.appendChild(style);
+      };
+  
+      let html = program.html || "";
+      window[`program-html-${p}`] = html; // temporary until a better solution is found (possibly adding into the script)
+  
+      let js = program.script;
+      eval(js);
+  
+      meowos.log("S", "PROGRAM LOADER", `Loaded '${p}'`);
+      loaded++;
+    },
+    error: function (request, status, error) {
+      if (meowos.error) return;
+
+      var message = "NO_ERROR";
+      if (request.status === 0 && !window.navigator.onLine) {
+        message = "INTERNET_DISCONNECTED";
+      } else if (request.status == 404) {
+        message = "RESOURCE_NOT_FOUND"; // Hopefully to never see this
+      } else if (request.status == 500) {
+        message = "INTERNAL_API_ERROR";
+      } else if (status == "timeout") {
+        message = `TIMEOUT_REACHED_LOADING_${p.toUpperCase()}`;
+      } else if (status == "error") {
+        message = "CONNECTION_TO_API_FAILED";
+      }
+
+      meowos.ErrorHandler("PROGRAM LOADER", `Failed to load '${p}' due to: ${message}`, true);
     }
-
-    let html = program.html || "";
-    window[`program-html-${p}`] = html; // temporary until a better solution is found (possibly adding into the script)
-
-    let js = program.script;
-    eval(js);
-
-    loaded++;
   });
-  console.log(`Loading ${p}...`);
 });
 
 window.onload = function () {
+  meowos.log("i", "SYSTEM", `MeowOS v${meowos.system.version} is now loading...`);
   loaded++;
 };
 setTimeout(function () {
@@ -50,9 +166,14 @@ _("overlay-login-username").select();
 let ONLOAD = function () {
   if (loaded !== requiredToLoad) return;
   if (!Auth.token) return;
+  
   clearInterval(ONLOADINT);
-  let ver = "0.1.0"; //TODO: include proper versioning
-  document.getElementsByTagName("title")[0].innerHTML = "LeafletOS v" + ver;
+
+  if (meowos.error) return;
+
+  meowos.log("S", "SYSTEM", `MeowOS v${meowos.system.version} has loaded!`);
+
+  document.getElementsByTagName("title")[0].innerHTML = "MeowOS | v" + meowos.system.version;
 
   document.onclick = function (e) {
     if (e.target.id !== "context-menu")
@@ -79,6 +200,8 @@ let ONLOAD = function () {
   }; //TODO: hide menu when clicked outside of it
 
   setInterval(function () {
+    if (meowos.error) return;
+
     let date = new Date();
     let clockTime =
       date.toLocaleTimeString() + "<br>" + date.toLocaleDateString();
@@ -87,6 +210,8 @@ let ONLOAD = function () {
 
   // Detect if windows are off-screen and move them into the screen.
   setInterval(function () {
+    if (meowos.error) return;
+
     WINDOWS.forEach((W) => {
       if (W.offscreen()) {
         W.window.style.top = "0px";
